85d04e10f397da427689f45bce5b13760127b1c9
ncalvertuk@gmail.com <ncalvertuk@Nicks-MacBook-Pro.local>
2020-05-04 17:47:16 +0100

    Updated links

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1-0C52fanztLHKh964YWzF6yUK51V7s86'
+export_file_name = 'stage-1-34'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Komondor','Mop']
 path = Path(__file__).parent
 
 app = Starlette()
